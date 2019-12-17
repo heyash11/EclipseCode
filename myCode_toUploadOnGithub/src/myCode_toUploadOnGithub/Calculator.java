57e4d4c4e98ec0f0513f26e153aadfa0b7b90360
Ashhu11 <Ashhu11@DESKTOP-ERNMGQ3>
2019-12-17 18:01:11 +0530

    First Commit
    uyploading all the file

---
+++ myCode_toUploadOnGithub/src/myCode_toUploadOnGithub/Calculator.java
@@ -0,0 +1,17 @@
+package myCode_toUploadOnGithub;
+
+public class Calculator {
+
+		public int add(int i,int j) {
+			int k=i+j;
+			return k;
+		}
+		
+		public int sub(int i,int j) {
+			int k=i-j;
+			return k;
+		}
+
+	}
+
+
---
+++ myCode_toUploadOnGithub/src/myCode_toUploadOnGithub/alien.java
@@ -0,0 +1,8 @@
+package myCode_toUploadOnGithub;
+
+public class alien {
+	public static void main(String[] args) {
+		System.out.println("Hey im Ashhu");
+	}
+
+}
